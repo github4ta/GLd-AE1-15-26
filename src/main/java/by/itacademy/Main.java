254eca3a9480733c8b2eef0761380d040fb8dbfe
Silantyeva Yekaterina <katyatest97@gmail.com>
2026-03-25 22:57:24 +0400

    refactor add to Main method if construction

--- src/main/java/by/itacademy/Main.java
+++ src/main/java/by/itacademy/Main.java
@@ -5,5 +5,10 @@ public class Main {
         System.out.println("Hello, Git!");
         int x = 24;
         int y = 13;
+        if(x > y ){
+            System.out.println("x больше y");
+        }else{
+            System.out.println("y больше х");
+        }
     }
 }
