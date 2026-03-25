1d6840328a6e426052987c19b51c71276d1a806c
TarasevichOA <tarasevicholga2014@gmail.com>
2026-03-25 21:47:21 +0300

    refactor add x, y to main method

--- src/main/java/by/itacademy/Main.java
+++ src/main/java/by/itacademy/Main.java
@@ -3,5 +3,7 @@ package by.itacademy;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
+        int x = 24;
+        int y = 13;
     }
 }
