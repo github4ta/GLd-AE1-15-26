b732c8cfc4f3d318ff659ad2fe924f0331de7a0f
Zarrina <zarina6991@gmail.com>
2026-03-31 00:25:09 +0700

    add HomePage

---
+++ src/main/java/by/onliner/HomePage.java
@@ -0,0 +1,30 @@
+package by.onliner;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePage {
+    private final String URL = "https://www.onliner.by/";
+    private final String INPUT_SEARCH = "//*[@id=\"fast-search\"]/div/input";
+    private final String BUTTON_AUTH = "//*[@id=\"userbar\"]/div[2]/div/div/div[1]";
+
+    private ChromeDriver driver;
+
+    public HomePage(ChromeDriver driver){
+        this.driver = driver;
+    }
+
+    public  void clickButtonAuth(){
+        driver.findElement(By.xpath(BUTTON_AUTH)).click();
+    }
+
+    public void open(){
+        driver.get(URL);
+    }
+    public void setInputSearch(String text){
+        driver.findElement(By.xpath(INPUT_SEARCH)).sendKeys(text);
+    }
+
+
+}
