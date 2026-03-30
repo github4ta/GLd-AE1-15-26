cbfedc5216df3e7d212e68c4f25dec19a90d038a
Klimava_Ekaterina <klimova.ek.ser@yandexby>
2026-03-30 22:03:37 +0300

    add first test

--- src/main/java/by/onliner/HomePage.java
+++ src/main/java/by/onliner/HomePage.java
@@ -4,25 +4,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomePage {
-	private final String URL = "https://www.onliner.by/";
-	private final String INPUT_SEARCH = "//*[@id=\"fast-search\"]/div/input";
-	private final String BUTTON_AUTH = "//*[@id=\"userbar\"]/div[2]/div/div/div[1]";
+    private final String URL = "https://www.onliner.by/";
+    private final String INPUT_SEARCH = "//*[@id=\"fast-search\"]/div/input";
+    private final String BUTTON_AUTH = "//*[@id=\"userbar\"]/div[2]/div/div/div[1]";
+    private final String COPY_RIGHTS = "/html/body/div[4]/footer/div/div/div/div[2]/div[2]";
 
-	private ChromeDriver driver;
+    private ChromeDriver driver;
 
-	public HomePage(ChromeDriver driver) {
-		this.driver = driver;
-	}
+    public HomePage(ChromeDriver driver) {
+        this.driver = driver;
+    }
 
-	public void open() {
-		driver.get(URL);
-	}
+    public void open() {
+        driver.get(URL);
+    }
 
-	public void clickButtonAuth() {
-		driver.findElement(By.xpath(BUTTON_AUTH)).click();
-	}
+    public void clickButtonAuth() {
+        driver.findElement(By.xpath(BUTTON_AUTH)).click();
+    }
 
-	public void setInputSearch(String text) {
-		driver.findElement(By.xpath(INPUT_SEARCH)).sendKeys(text);
-	}
+    public void setInputSearch(String text) {
+        driver.findElement(By.xpath(INPUT_SEARCH)).sendKeys(text);
+    }
+
+    public String getCopyRights() {
+        return driver.findElement(By.xpath(COPY_RIGHTS)).getText();
+    }
 }
---
+++ src/test/java/by/onliner/HomeTest.java
@@ -0,0 +1,18 @@
+package by.onliner;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeTest {
+    @Test
+    public void test1(){
+        ChromeDriver driver = new ChromeDriver();
+        HomePage homePage = new HomePage(driver);
+
+        homePage.open();
+        String textActualCopyRights = homePage.getCopyRights();
+
+        Assertions.assertEquals("© 2001—2026 Onlíner", textActualCopyRights);
+    }
+}
