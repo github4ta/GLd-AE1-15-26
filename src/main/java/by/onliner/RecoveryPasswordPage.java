b59ad19d1de84ac0caa02014cf8c291025a8e63a
Zarrina <zarina6991@gmail.com>
2026-04-01 20:58:32 +0700

    add RecoveryPasswordPage

---
+++ src/main/java/by/onliner/RecoveryPasswordPage.java
@@ -0,0 +1,33 @@
+package by.onliner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RecoveryPasswordPage {
+
+private final  String  TITLE = "//*[@id=\"container\"]/div/div/div/div/div[2]/form/div[1]";
+private final  String  INPUT_EMAIL = "//*[@id=\"container\"]/div/div/div/div/div[2]/form/div[3]/div/div/div/div[1]/div/div/div/div/input";
+private final  String  BUTTON_AUTH = "//*[@id=\"container\"]/div/div/div/div/div[2]/form/div[4]/button";
+
+private WebDriver driver;
+
+public RecoveryPasswordPage(ChromeDriver driver){
+
+    this.driver = driver;
+}
+
+public String getTitleText(){
+    return driver.findElement(By.xpath(TITLE)).getText();
+}
+
+public void  setInputEmail(String email){
+
+    driver.findElement(By.xpath(INPUT_EMAIL)).sendKeys(email);
+}
+
+public void clickButtonAuth(){
+    driver.findElement(By.xpath(BUTTON_AUTH)).click();
+}
+
+}
