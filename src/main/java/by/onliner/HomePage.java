8f4f638753a9de8d70f059f935299a59bb2bed43
Zarrina <zarina6991@gmail.com>
2026-04-01 21:38:41 +0700

    add HomeTest

--- src/main/java/by/onliner/HomePage.java
+++ src/main/java/by/onliner/HomePage.java
@@ -1,6 +1,5 @@
 package by.onliner;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,23 +7,28 @@ public class HomePage {
     private final String URL = "https://www.onliner.by/";
     private final String INPUT_SEARCH = "//*[@id=\"fast-search\"]/div/input";
     private final String BUTTON_AUTH = "//*[@id=\"userbar\"]/div[2]/div/div/div[1]";
+    public final String COPY_RIGHTS = "/html/body/div[3]/footer/div/div/div/div[2]/div[2]";
 
     private ChromeDriver driver;
 
-    public HomePage(ChromeDriver driver){
+    public HomePage(ChromeDriver driver) {
         this.driver = driver;
     }
 
-    public  void clickButtonAuth(){
+    public void clickButtonAuth() {
         driver.findElement(By.xpath(BUTTON_AUTH)).click();
     }
 
-    public void open(){
+    public void open() {
         driver.get(URL);
     }
-    public void setInputSearch(String text){
+
+    public void setInputSearch(String text) {
+
         driver.findElement(By.xpath(INPUT_SEARCH)).sendKeys(text);
     }
 
-
+    public String getCopyRights(){
+        return driver.findElement(By.xpath(COPY_RIGHTS)).getText();
+    }
 }
---
+++ src/test/java/by/onliner/HomeTest.java
@@ -0,0 +1,20 @@
+package by.onliner;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeTest {
+
+    @Test
+    public void test1() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        HomePage homePage = new HomePage(driver);
+
+        homePage.open();
+        String textActualCopyRights = homePage.getCopyRights();
+
+        Assertions.assertEquals("© 2001—2026 Onlíner", textActualCopyRights);
+    }
+}
