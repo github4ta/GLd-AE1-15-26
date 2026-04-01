6f85045e19dee9707dc72987d3e4925622b3f2da
Klimava_Ekaterina <klimova.ek.ser@yandexby>
2026-04-01 21:16:35 +0300

    add test RG003

--- src/test/java/by/onliner/RegistrationTest.java
+++ src/test/java/by/onliner/RegistrationTest.java
@@ -1,4 +1,23 @@
 package by.onliner;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class RegistrationTest {
+
+    @Test
+    public void testRG003(){
+        ChromeDriver driver = new ChromeDriver();
+        HomePage homePage = new HomePage(driver);
+        AuthFormPage auth = new AuthFormPage(driver);
+        RegistrationPage registration = new RegistrationPage(driver);
+
+        homePage.open();
+        homePage.clickButtonAuth();
+
+        auth.clickLinkRegistor();
+
+
+
+    }
 }
