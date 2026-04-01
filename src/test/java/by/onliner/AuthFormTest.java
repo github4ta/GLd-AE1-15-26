94b7fc8e36ee4031bc46131ebcaebfd3ae6e352f
Irina Menshova <menshovaiv@gmail.com>
2026-04-01 21:13:39 +0300

    add AF002 task

--- src/test/java/by/onliner/AuthFormTest.java
+++ src/test/java/by/onliner/AuthFormTest.java
@@ -1,4 +1,27 @@
 package by.onliner;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AuthFormTest {
+	@Test
+	public void testAF002(){
+		ChromeDriver driver = new ChromeDriver();
+		HomePage homePage = new HomePage(driver);
+
+		homePage.open();
+		AuthFormPage authFormPage = new AuthFormPage(driver);
+		driver.manage().window().maximize();
+
+		homePage.clickButtonAuth();
+		authFormPage.setInputName("test@test.com");
+		authFormPage.setInputPassword("");
+		authFormPage.clickButtonSubmit();
+
+		String textActualError = authFormPage.getErrorPasswordMessageText();
+
+		Assertions.assertEquals("Укажите пароль", textActualError);
+		driver.quit();
+	}
 }
