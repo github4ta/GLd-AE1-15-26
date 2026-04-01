553b536bef4048becd849ff1670edef73e3b39e7
Angelika Dasiukevich <dasiukevich.a@gmail.com>
2026-04-01 21:12:08 +0300

    add RG001

--- src/main/java/by/onliner/RegistrationPage.java
+++ src/main/java/by/onliner/RegistrationPage.java
@@ -10,6 +10,9 @@ public class RegistrationPage  extends ParentPage {
     private final String INPUT_REPEAT_PASSWORD = "//*[@id=\"container\"]/div/div/div/form/div[2]/div/div[8]/div/div/div/div/input";
     private final String BUTTON_SUBMIT = "//*[@id=\"container\"]/div/div/div/form/div[2]/div/div[9]/button";
     private final String LINK_LOGIN = "//*[@id=\"container\"]/div/div/div/form/div[2]/div/div[10]/a";
+    private final String ERROR_MESSAGE_EMAIL = "//*[@id=\"container\"]/div/div/div/form/div[2]/div/div[5]/div/div/div[2]/div";
+    private final String ERROR_MESSAGE_PASSWORD = "//*[@id=\"container\"]/div/div/div/form/div[2]/div/div[6]/div/div/div[2]/div";
+    private final String ERROR_MESSAGE_REPEAT_PASSWORD = "//*[@id=\"container\"]/div/div/div/form/div[2]/div/div[8]/div/div/div[2]/div";
 
     public RegistrationPage(ChromeDriver driver) {
         this.driver = driver;
@@ -37,5 +40,17 @@ public class RegistrationPage  extends ParentPage {
 
     public void clickLinkLogin() {
         driver.findElement(By.xpath(LINK_LOGIN)).click();
+    }
+
+    public String getErrorMessageEmail() {
+        return driver.findElement(By.xpath(ERROR_MESSAGE_EMAIL)).getText();
+    }
+
+    public String getErrorMessagePassword() {
+        return driver.findElement(By.xpath(ERROR_MESSAGE_PASSWORD)).getText();
+    }
+
+    public String getErrorMessageRepeatPassword() {
+        return driver.findElement(By.xpath(ERROR_MESSAGE_REPEAT_PASSWORD)).getText();
     }
 }
--- src/test/java/by/onliner/RegistrationTest.java
+++ src/test/java/by/onliner/RegistrationTest.java
@@ -1,4 +1,28 @@
 package by.onliner;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class RegistrationTest {
+    @Test
+    public void testRG001() {
+        ChromeDriver driver = new ChromeDriver();
+        HomePage homePage = new HomePage(driver);
+        driver.manage().window().maximize();
+
+        homePage.open();
+        homePage.clickButtonAuth();
+
+        AuthFormPage authFormPage = new AuthFormPage(driver);
+        authFormPage.clickLinkRegistor();
+
+        RegistrationPage registrationPage = new RegistrationPage(driver);
+        registrationPage.clickButtonSubmit();
+
+        Assertions.assertEquals("Укажите e-mail", registrationPage.getErrorMessageEmail());
+        Assertions.assertEquals("Укажите пароль", registrationPage.getErrorMessagePassword());
+        Assertions.assertEquals("Укажите пароль", registrationPage.getErrorMessageRepeatPassword());
+    }
 }
