0fa37bf228ca552b220edb91db625bb9eadb0719
TarasevichOA <tarasevicholga2014@gmail.com>
2026-04-03 23:01:41 +0300

    add method getErrorMessageNotRegister to class RecoverPasswordPage add testRE004 RecoverPasswordTest

--- src/main/java/by/onliner/RecoverPasswordPage.java
+++ src/main/java/by/onliner/RecoverPasswordPage.java
@@ -2,14 +2,21 @@ package by.onliner;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class RecoverPasswordPage extends ParentPage {
     private final String TITLE = "//*[@id=\"container\"]/div/div/div/div/div[2]/form/div[1]";
     private final String INPUT_EMAIL = "//*[@id=\"container\"]/div/div/div/div/div[2]/form/div[3]/div/div/div/div[1]/div/div/div/div/input";
     private final String BUTTON_AUTH = "//*[@id=\"container\"]/div/div/div/div/div[2]/form/div[4]/button";
 
+    private final String ERROR_MESSAGE_NOT_REGISTER = "//*[@id=\"container\"]/div/div/div/div/div[2]/form/div[3]/div/div/div/div[2]/div";
+
     public RecoverPasswordPage(ChromeDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public String getTitleText() {
@@ -22,5 +29,10 @@ public class RecoverPasswordPage extends ParentPage {
 
     public void clickButtonAuth() {
         driver.findElement(By.xpath(BUTTON_AUTH)).click();
+    }
+
+    public String getErrorMessageNotRegister(){
+        //return driver.findElement(By.xpath(ERROR_MESSAGE_NOT_REGISTER)).getText();
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(ERROR_MESSAGE_NOT_REGISTER))).getText();
     }
 }
--- src/test/java/by/onliner/RecoverPasswordTest.java
+++ src/test/java/by/onliner/RecoverPasswordTest.java
@@ -1,4 +1,27 @@
 package by.onliner;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class RecoverPasswordTest {
+
+    @Test
+    public void testRE004(){
+        ChromeDriver driver = new ChromeDriver();
+        HomePage homePage = new HomePage(driver);
+        homePage.open();
+        driver.manage().window().maximize();
+        homePage.clickButtonAuth();
+
+        AuthFormPage authFormPage = new AuthFormPage(driver);
+        authFormPage.clickLinkForgotPassword();
+
+        RecoverPasswordPage recoverPasswordPage = new RecoverPasswordPage(driver);
+        recoverPasswordPage.setInputEmail("user@user.com");
+        recoverPasswordPage.clickButtonAuth();
+
+        Assertions.assertEquals("Такой пользователь не зарегистрирован", recoverPasswordPage.getErrorMessageNotRegister());
+        driver.quit();
+    }
 }
