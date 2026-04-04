60982a548b92b4b46452784d26d01fb7c11f7ff9
Silantyeva Yekaterina <katyatest97@gmail.com>
2026-04-05 02:05:38 +0400

    refactor: initialize WebDriver setup for RegistrationTest

--- src/test/java/by/onliner/RegistrationTest.java
+++ src/test/java/by/onliner/RegistrationTest.java
@@ -1,6 +1,28 @@
 package by.onliner;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class RegistrationTest {
+
+    private  ChromeDriver driver;
+
+    @BeforeEach
+    public  void setup(){
+        System.setProperty("webdriver.chrome.driver", "/home/ekaterina/Desktop/drivers/chromedriver-linux64/chromedriver");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.setBinary("/home/ekaterina/Desktop/drivers/chrome-linux64/chrome");
+
+        driver = new ChromeDriver(options);
+    }
 
     @Test
     public void testRG(){
@@ -9,7 +31,7 @@ public class RegistrationTest {
 
     @Test
     public void testRG003(){
-        ChromeDriver driver = new ChromeDriver();
+
         HomePage homePage = new HomePage(driver);
         AuthFormPage auth = new AuthFormPage(driver);
         RegistrationPage registration = new RegistrationPage(driver);
