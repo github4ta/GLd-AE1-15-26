0510bbb55facbec984aa7d7e6a2fea1385941014
Irina Menshova <menshovaiv@gmail.com>
2026-04-04 13:28:50 +0300

    add test RE001

--- src/main/java/by/onliner/RecoverPasswordPage.java
+++ src/main/java/by/onliner/RecoverPasswordPage.java
@@ -4,15 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 public class RecoverPasswordPage extends ParentPage {
     private final String TITLE = "//*[@id=\"container\"]/div/div/div/div/div[2]/form/div[1]";
     private final String INPUT_EMAIL = "//*[@id=\"container\"]/div/div/div/div/div[2]/form/div[3]/div/div/div/div[1]/div/div/div/div/input";
     private final String BUTTON_AUTH = "//*[@id=\"container\"]/div/div/div/div/div[2]/form/div[4]/button";
-
     private final String ERROR_MESSAGE_NOT_REGISTER = "//*[@id=\"container\"]/div/div/div/div/div[2]/form/div[3]/div/div/div/div[2]/div";
+    private final String EXTENDED_DESCRIPTION_ERROR = "//*[@id=\"container\"]/div/div/div/div/div[2]/form/div[3]/div/div/div/div[1]/div/div/div[2]/div";
 
     public RecoverPasswordPage(ChromeDriver driver) {
         this.driver = driver;
@@ -32,7 +31,10 @@ public class RecoverPasswordPage extends ParentPage {
     }
 
     public String getErrorMessageNotRegister(){
-        //return driver.findElement(By.xpath(ERROR_MESSAGE_NOT_REGISTER)).getText();
         return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(ERROR_MESSAGE_NOT_REGISTER))).getText();
+    }
+
+    public String getExtendedDescriptionError() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(EXTENDED_DESCRIPTION_ERROR))).getText();
     }
 }
