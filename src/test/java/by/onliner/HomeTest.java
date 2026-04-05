ee1bc4408d97e424135ba0139d432a6c427c6169
Irina <menshovaiv@gmail.com>
2026-04-05 11:27:37 +0300

    add task HP002

--- src/test/java/by/onliner/HomeTest.java
+++ src/test/java/by/onliner/HomeTest.java
@@ -15,4 +15,16 @@ public class HomeTest {
 
         Assertions.assertEquals("© 2001—2026 Onlíner", textActualCopyRights);
     }
+
+    @Test
+    public void HP002(){
+        ChromeDriver driver = new ChromeDriver();
+        HomePage homePage = new HomePage(driver);
+
+        homePage.open();
+        driver.manage().window().maximize();
+
+        String textActualButtonAuth = homePage.getButtonAuthText();
+        Assertions.assertEquals("Вход", textActualButtonAuth);
+    }
 }
