c464c12e3f4079935cb9ca5642bfe8590c5e2f03
Zarrina <zarina6991@gmail.com>
2026-04-06 23:45:08 +0700

    fix comment

--- src/main/java/by/onliner/RecoverPasswordPage.java
+++ src/main/java/by/onliner/RecoverPasswordPage.java
@@ -15,6 +15,8 @@ public class RecoverPasswordPage extends ParentPage {
     private final String EXTENDED_DESCRIPTION_ERROR = "//*[@id=\"container\"]/div/div/div/div/div[2]/form/div[3]/div/div/div/div[1]/div/div/div[2]/div";
     private final String SHOW_USER_NOT_FINISHED_REGISTER = "//*[@id=\"container\"]/div/div/div/div/div[2]/form/div[3]/div/div/div/div[2]/div";
 
+    private  final String URL_RECOVER_PASSWORD = "https://profile.onliner.by/recover-password";
+
     public RecoverPasswordPage(ChromeDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -29,7 +31,7 @@ public class RecoverPasswordPage extends ParentPage {
     }
 
     public void open() {
-        driver.get("https://profile.onliner.by/recover-password");
+        driver.get(URL_RECOVER_PASSWORD);
     }
 
     public void enterUsername(String text) {
