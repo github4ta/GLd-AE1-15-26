3b6d500cc7bfca3c10b2501277d8d6f5eeb8278f
Sergei Tsarik <49017393+github4ta@users.noreply.github.com>
2026-04-08 21:41:08 +0200

    RG004

--- src/test/java/by/onliner/RegistrationTest.java
+++ src/test/java/by/onliner/RegistrationTest.java
@@ -10,6 +10,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class RegistrationTest {
 
     private  ChromeDriver driver;
@@ -25,6 +28,26 @@ public class RegistrationTest {
         options.setBinary("/home/ekaterina/Desktop/drivers/chrome-linux64/chrome");
 
         driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void testRG004() {
+        ChromeDriver driver = new ChromeDriver();
+        HomePage homePage = new HomePage(driver);
+
+        homePage.open();
+        homePage.clickButtonAuth();
+
+        AuthFormPage authFormPage = new AuthFormPage(driver);
+        authFormPage.clickLinkRegistor();
+
+        RegistrationPage registrationPage = new RegistrationPage(driver);
+        registrationPage.setInputRepeatPassword("1234dfgDRF@$1313131");
+        registrationPage.clickButtonSubmit();
+
+        Assertions.assertEquals("", registrationPage.getErrorMessageEmail());
+        Assertions.assertEquals("", registrationPage.getErrorMessagePassword());
+        Assertions.assertEquals("", registrationPage.getErrorMessageRepeatPassword());
     }
 
     @Test
