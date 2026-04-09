28ddb6671dc76776fa5fd5039be1dd920fb8ec57
Sergei Tsarik <serge.tsarik@gmail.com>
2026-04-09 21:23:25 +0300

    add search po and test

---
+++ src/main/java/by/onliner/SearchPage.java
@@ -0,0 +1,11 @@
+package by.onliner;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SearchPage extends ParentPage {
+
+    public SearchPage(ChromeDriver driver) {
+        this.driver = driver;
+    }
+
+}
---
+++ src/test/java/by/onliner/SearchTest.java
@@ -0,0 +1,5 @@
+package by.onliner;
+
+public class SearchTest {
+
+}
