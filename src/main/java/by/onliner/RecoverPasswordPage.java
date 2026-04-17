bb659bd54c1fc93ea9bf83e7b7b127d378d537e0
Klimava_Ekaterina <klimova.ek.ser@yandexby>
2026-04-17 22:28:53 +0300

    refactor RecoverPasswordPage and RecoverPasswordText

--- src/main/java/by/onliner/RecoverPasswordPage.java
+++ src/main/java/by/onliner/RecoverPasswordPage.java
@@ -1,7 +1,8 @@
 package by.onliner;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,14 +12,14 @@ public class RecoverPasswordPage extends BasePage {
     private final String TITLE = "//div[contains(@class, 'auth-form__title')]";
     private final String INPUT_EMAIL = "//input[@placeholder='Ник, e-mail или номер телефона']";
     private final String BUTTON_AUTH = "//button[@type='submit']";
-    private final String UNREGISTRED_USER_ERROR = "//div[contains(text(), 'Такой пользователь не зарегистрирован')]";
     private final String ERROR_MESSAGE_NOT_REGISTER = "//div[contains(text(), 'Такой пользователь не зарегистрирован')]";
     private final String EXTENDED_DESCRIPTION_ERROR = "//div[contains(text(), 'Укажите ник, e-mail или номер телефона')]";
-    private final String SHOW_USER_NOT_FINISHED_REGISTER = "//div[contains(text(), 'Такой пользователь не зарегистрирован')]";
+    private final String SHOW_USER_NOT_FINISHED_REGISTER = "//div[contains(text(), 'Вы не завершили процесс регистрации')]";
+    private final String COOKIES = "//*[@id=\"submit-button\"]";
 
     private final String URL_RECOVER_PASSWORD = "https://profile.onliner.by/recover-password";
 
-    public RecoverPasswordPage(ChromeDriver driver) {
+    public RecoverPasswordPage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
@@ -35,17 +36,15 @@ public class RecoverPasswordPage extends BasePage {
         driver.get(URL_RECOVER_PASSWORD);
     }
 
-    public void enterUsername(String text) {
-        driver.findElement(By.xpath(INPUT_EMAIL)).clear();
-        driver.findElement(By.xpath(INPUT_EMAIL)).sendKeys(text);
+    public void clickCookies() {
+        WebDriverWait wait  = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement cookies = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(COOKIES)));
+        cookies.click();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(COOKIES)));
     }
 
     public void clickButtonAuth() {
         driver.findElement(By.xpath(BUTTON_AUTH)).click();
-    }
-
-    public String unregistredUserError() {
-        return driver.findElement(By.xpath(UNREGISTRED_USER_ERROR)).getText();
     }
 
     public String getErrorMessageNotRegister() {
@@ -57,6 +56,6 @@ public class RecoverPasswordPage extends BasePage {
     }
 
     public String getShowUserNotFinishedRegister() {
-        return driver.findElement(By.xpath(SHOW_USER_NOT_FINISHED_REGISTER)).getText();
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(SHOW_USER_NOT_FINISHED_REGISTER))).getText();
     }
 }
